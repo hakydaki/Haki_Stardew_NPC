df12b31b5777e00bbf624a649c84801102f6512c
hakydaki <78766301+hakydaki@users.noreply.github.com>
2024-07-08 04:40:49 -0700

    connecting more dialogue. quick sprite fixes

--- [CP] Haki_Stardew/retro_furniture.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="retro_furniture" tilewidth="32" tileheight="32" tilecount="48" columns="6">
- <image source="E:/SteamLibrary/steamapps/common/Stardew Valley/Content (unpacked)/TileSheets/retro_furniture.png" width="208" height="265"/>
-</tileset>
